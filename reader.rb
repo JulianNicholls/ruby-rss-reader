b88082c502d2358f1c3825e2381001bd1c69369b
Julian Nicholls <juliannicholls29@gmail.com>
2015-11-29 18:45:13 +0000

    Basic Aggregator

--- reader.rb
+++ reader.rb
@@ -2,54 +2,74 @@
 
 require 'open-uri'
 require 'nokogiri'
-require 'pp'
-require 'humantime'
 
-TOP_LEVEL = '//channel'
-ITEMS     = '//item'
+class Feed
+  TOP_LEVEL = '//channel'
+  ITEMS     = '//item'
 
-INFO_PARTS = {
-  title:         'title',
-  link:          'link',
-  description:   'description',
-  copyright:     'copyright',
-  image_url:     'image/url',
-  image_caption: 'image/title',
-  image_width:   'image/width',
-  image_height:  'image/height'
-}
+  INFO_PARTS = {
+    title:         'title',
+    link:          'link',
+    description:   'description',
+    copyright:     'copyright',
+    image_url:     'image/url',
+    image_caption: 'image/title',
+    image_width:   'image/width',
+    image_height:  'image/height'
+  }
 
-ITEM_PARTS = {
-  title: 'title',
-  description: 'description',
-  link: 'link',
-  timestamp: 'pubDate'
-}
+  ITEM_PARTS = {
+    title:       'title',
+    description: 'description',
+    link:        'link',
+    timestamp:   'pubDate'
+  }
 
-rss     = Nokogiri::XML(open ARGV[0] || 'bbc_rss_feed.xml')
-info    = rss.xpath(TOP_LEVEL)
-items   = rss.xpath(ITEMS)
+  def initialize(feed_path)
+    @rss = Nokogiri::XML(open feed_path)  # File or URL, could raise
+  end
 
-def traverse(feed, parts)
-  parts.each_with_object({}) do |(key, path), object|
-    item        = feed.xpath(path)
-    object[key] = item.children.to_s unless item.empty?
+  def info
+    @info ||= get_info
+  end
+
+  def get_info
+    info = @rss.xpath(TOP_LEVEL)
+    traverse(info, INFO_PARTS)
+  end
+
+  def items
+    @items ||= get_items
+  end
+
+  def get_items
+    items   = @rss.xpath(ITEMS)
+    feed_items = items.each_with_object([]) do |item, array|
+      array << traverse(item, ITEM_PARTS)
+    end
+  end
+
+  private
+
+  def traverse(base, parts)
+    parts.each_with_object({}) do |(key, path), object|
+      item        = base.xpath(path)
+      object[key] = item.children.to_s unless item.empty?
+    end
   end
 end
 
-info = traverse(info, INFO_PARTS)
+if $PROGRAM_NAME == __FILE__
+  require 'pp'
 
-feed_items = items.each_with_object([]) do |item, array|
-  array << traverse(item, ITEM_PARTS)
+  begin
+    addr = ARGV[0] || 'bbc_rss_feed.xml'
+    feed = Feed.new(addr)
+
+    pp feed.info
+    pp feed.items.count
+    pp feed.items[0]
+  rescue StandardError => e
+    warn "Cannot open #{addr}: #{e}"
+  end
 end
-
-pp info
-pp items.count
-pp feed_items.count
-pp feed_items[0]
-# puts
-# pp info[:title]
-# puts
-# pp info[:title].children
-# puts
-# pp info[:title].children.to_s
--- webmain.rb
+++ webmain.rb
@@ -1,13 +1,39 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'sass'
+require 'tilt/erb'
+require './reader'
 
 # RSS display application
 class RssApp < Sinatra::Application
   get('/css/style.css') { scss :style }
 
   get('/') do
-    @title = 'BBC RSS Feed'
+    @title = 'Home'
     erb :index
+  end
+
+  get('/feed/:addr') do
+    @title = 'Feed'
+
+    begin
+      puts "Addr: #{params[:addr]}"
+      feed = Feed.new params[:addr] || 'bbc_rss_feed.xml'
+      @title = feed.info[:title]
+      @info  = feed.info
+      @items = feed.items
+    rescue StandardError => e
+      @title = "Cannot load feed at #{params[:addr]}"
+      @info = {
+        description:  e
+      }
+
+      @items = []
+      warn @title
+      warn e
+    end
+
+    puts "Loading..."
+    erb :feed
   end
 end
