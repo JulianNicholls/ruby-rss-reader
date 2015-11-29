e486e1f694e0bfef696467f2203532996aab380a
Julian Nicholls <juliannicholls29@gmail.com>
2015-11-29 17:42:33 +0000

    Save before class'ing

--- reader.rb
+++ reader.rb
@@ -3,44 +3,50 @@
 require 'open-uri'
 require 'nokogiri'
 require 'pp'
+require 'humantime'
 
 TOP_LEVEL = '//channel'
-ITEMS     = TOP_LEVEL + '/item'
+ITEMS     = '//item'
 
 INFO_PARTS = {
-  title:         '/title',
-  link:          '/link',
-  description:   '/description',
-  copyright:     '/copyright',
-  image_url:     '/image/url',
-  image_caption: '/image/title',
-  image_width:   '/image/width',
-  image_height:  '/image/height'
+  title:         'title',
+  link:          'link',
+  description:   'description',
+  copyright:     'copyright',
+  image_url:     'image/url',
+  image_caption: 'image/title',
+  image_width:   'image/width',
+  image_height:  'image/height'
 }
 
 ITEM_PARTS = {
-  title: '/title',
-  description: '/description',
-  link: '/link',
-  timestamp: '/pubDate'
+  title: 'title',
+  description: 'description',
+  link: 'link',
+  timestamp: 'pubDate'
 }
 
-rss     = Nokogiri::XML(open 'bbc_rss_feed.xml')
+rss     = Nokogiri::XML(open ARGV[0] || 'bbc_rss_feed.xml')
+info    = rss.xpath(TOP_LEVEL)
 items   = rss.xpath(ITEMS)
 
-def traverse(feed, parts, base = TOP_LEVEL)
+def traverse(feed, parts)
   parts.each_with_object({}) do |(key, path), object|
-    item = feed.xpath(base + path)
+    item        = feed.xpath(path)
     object[key] = item.children.to_s unless item.empty?
   end
 end
 
-info = traverse(rss, INFO_PARTS)
+info = traverse(info, INFO_PARTS)
 
-items.each_with_object
+feed_items = items.each_with_object([]) do |item, array|
+  array << traverse(item, ITEM_PARTS)
+end
 
 pp info
 pp items.count
+pp feed_items.count
+pp feed_items[0]
 # puts
 # pp info[:title]
 # puts
