f88ea101e083f4c520dddbb2fe10d9c8f7c26350
Julian Nicholls <juliannicholls29@gmail.com>
2015-11-30 11:07:35 +0000

    Correct feed loading
    Add initial article display and styling

--- webmain.rb
+++ webmain.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'sass'
 require 'tilt/erb'
+require 'tilt/sass'
 require './reader'
 
 # RSS display application
@@ -13,15 +14,16 @@ class RssApp < Sinatra::Application
     erb :index
   end
 
-  get('/feed/:addr') do
+  get('/feed/*') do
     @title = 'Feed'
+    addr = params[:splat][0].sub 'http:/', 'http://'
 
     begin
-      puts "Addr: #{params[:addr]}"
-      feed = Feed.new params[:addr] || 'bbc_rss_feed.xml'
+      puts "Addr: #{addr}"
+      feed = Feed.new addr || 'bbc_rss_feed.xml'
       @title = feed.info[:title]
       @info  = feed.info
-      @items = feed.items
+      @items = feed.items.sort_by { |item| Time.parse(item[:timestamp]) }.reverse
     rescue StandardError => e
       @title = "Cannot load feed at #{params[:addr]}"
       @info = {
@@ -34,6 +36,9 @@ class RssApp < Sinatra::Application
     end
 
     puts "Loading..."
+    p @info
+    p @items[0]
+    p @items[1]
     erb :feed
   end
 end
