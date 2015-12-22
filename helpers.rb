8b3fb8b9c6b1a5a2c103705bb8db8b6e6221868c
Julian Nicholls <juliannicholls29@gmail.com>
2015-12-22 22:00:45 +0000

    Add guard clause for linkify

--- helpers.rb
+++ helpers.rb
@@ -1,7 +1,7 @@
 # Helper functions for the RSS viewer
 module RssAppHelpers
   def linkify(text)
-    text.gsub(%r{(https?://\S+)}, '<a href="\1" target="_blank">\1</a>')
+    text.gsub(%r{(https?://\S+)}, '<a href="\1" target="_blank">\1</a>') if text
   end
 
   module_function :linkify
