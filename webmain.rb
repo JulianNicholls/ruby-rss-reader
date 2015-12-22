ad2a7c6e85ece823ac4cb2d71bacfcae5a8a7f61
Julian Nicholls <juliannicholls29@gmail.com>
2015-12-22 22:05:24 +0000

    Add feed list from Mongo feeds/sites

--- webmain.rb
+++ webmain.rb
@@ -7,10 +7,19 @@ require 'tilt/sass'
 require './reader'
 require './helpers'
 require './humantime'
+require './feedlist'
 
 # RSS display application
 class RssApp < Sinatra::Application
   include RssAppHelpers
+
+  def self.feeds
+    @feeds ||= FeedList.new
+  end
+
+  def feeds
+    self.class.feeds
+  end
 
   get('/css/style.css') { scss :style }
 
@@ -21,31 +30,19 @@ class RssApp < Sinatra::Application
 
   get('/feed/*') do
     @title = 'Feed'
-    addr = params[:splat][0].sub(%r{(https?):/}, '\1://')
+    @address = params[:splat][0].sub(%r{(https?):/}, '\1://')
 
     begin
-      # puts "Addr: #{addr}"
-      feed = Feed.new addr || 'bbc_rss_feed.xml'
+      feed = Feed.new @address || 'bbc_rss_feed.xml'
       @title = feed.info[:title]
       @info  = feed.info
-      @items = feed.items.sort_by do |item|
-        Time.parse(item[:timestamp])
-      end.reverse
+      @items = feed.items.sort_by { |item| Time.parse(item[:timestamp]) }.reverse
     rescue StandardError => err
-      @title = "Cannot load feed at #{params[:addr]}"
-      @info = {
-        description:  err
-      }
-
+      @title = "Cannot load feed at #{@address}"
+      @info  = { description:  err }
       @items = []
-      warn @title
-      warn err
     end
 
-    # puts "Loading..."
-    # p @info
-    # p @items[0]
-    # p @items[1]
     erb :feed
   end
 
