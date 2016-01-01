43dae76a85b4eb6464add2c914e8f7be5c8a3d06
Julian Nicholls <juliannicholls29@gmail.com>
2016-01-01 19:30:08 +0000

    Return empty string from #linkify on failure

--- helpers.rb
+++ helpers.rb
@@ -2,12 +2,14 @@
 module RssAppHelpers
   def linkify(text)
     if text && text.is_a?(String)
-      text.gsub(%r{(https?://\S+)}, '<a href="\1" target="_blank">\1</a>') if text
+      text.gsub(%r{(https?://\S+)}, '<a href="\1" target="_blank">\1</a>')
     else
       warn "NAS: #{text.inspect}"
+      ''
     end
   rescue => err
     warn "rescue: #{err.inspect}"
+    ''
   end
 
   module_function :linkify
