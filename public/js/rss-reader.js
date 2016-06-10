57e6995ee508098fdf3d0dbb6c6c866501336d51
Julian Nicholls <juliannicholls29@gmail.com>
2016-06-10 18:54:53 +0100

    width is now set explicitly for all item images.
    resize_images() is no longer necessary.

--- public/js/rss-reader.js
+++ public/js/rss-reader.js
@@ -1,6 +1,4 @@
 $(function() {
-    $(window).load(resize_images);
-
     setInterval(function () {
         $(".humantime").each(function () {
             var $time = $(this);
@@ -15,17 +13,3 @@ $(function() {
         window.location = '/feed/' + this.value;
     });
 });
-
-resize_images = function () {
-    console.log('Resize Images');
-
-    $(".image-col img").each(function () {
-        console.log(this.src);
-
-        var width = this.width;
-
-        if(width > 150) {
-            this.width = 150;
-        }
-    });
-}
