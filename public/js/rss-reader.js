4adf0ed2989ccbba251070a792634f68443ada2e
Julian Nicholls <juliannicholls29@gmail.com>
2017-02-08 11:27:21 +0000

    Rewrite JS reloading etc without jQuery

--- public/js/rss-reader.js
+++ public/js/rss-reader.js
@@ -1,31 +1,33 @@
-let handle = setInterval(updateTimestamps, 90000);
+const handle = setInterval(updateTimestamps, 90000);
 
 function updateTimestamps() {
-  let first = true;
+  let   first  = true;
+  const stamps = document.querySelectorAll('.humantime');
 
-  $(".humantime").each(function () {
-    var $time = $(this);
+  stamps.forEach(stampItem => {
+    fetch(`/humantime?stamp=${stampItem.dataset.stamp}&first=${first}`)
+      .then(response => {
+        if (response.status === 200) {
+          response.json().then(data => {
+            if (data.first && data.ago.match(/yesterday/i)) {
+              console.log('Cancelling update')
+              clearInterval(handle);    // Stop asking after the top one is yesterday
+            }
 
-    $.get('/humantime', { stamp: this.dataset.stamp, first: first }, function(data) {
-      data = JSON.parse(data);
-      if (data.first && data.ago.match(/yesterday/i)) {
-        console.log('Cancelling update')
-        clearInterval(handle);    // Stop asking after the top one is yesterday
-      }
-
-      $time.text(data.ago)
-    });
+            stampItem.textContent = data.ago;
+          });
+        }
+      });
 
     first = false;
   });
 }
 
-$(function() {
-  $("select#feeds").on('change', function () {
-    window.location = '/feed/' + this.value;
-  });
+const feeds  = document.querySelector('#feeds');
+const reload = document.querySelector('#reload');
 
-  $("#reload").on('click', function () {
-    window.location.reload(true);
-  });
+feeds.addEventListener('change', function() {
+  window.location = '/feed/' + this.value;
 });
+
+reload.addEventListener('click', () => window.location.reload(true));
